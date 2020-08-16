600fad2ef047631d33315e95ab6ebe7d8bf3718d
Eli Cohen <eliunited@gmail.com>
2020-08-16 03:24:14 -0700

    first commit

---
+++ pages/portfolio.js
@@ -0,0 +1,129 @@
+export default function Portfolio() {
+	return (
+		<section className="max-w-5xl mx-auto p-4 relative min-h-screen">
+			<div className="flex mb-10">
+				<div className="self-start sticky-top vertical mt-4 pt-4">
+					<h2 className="font-bold text-yellow-500">Projects</h2>
+				</div>
+				<div className="flex-col m-4">
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline hover:yellow-underline">
+							The Watson Fellowship
+						</h1>
+						<div className="flex flex-row">
+							<h3 className="text-left font-plex group-hover:italic">
+								2019 — 2020
+							</h3>
+						</div>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Colander World
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Discussion Collective
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic rotat">
+							2019 — 2020
+						</h3>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Annals of Extraction
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+				</div>
+			</div>
+			<div className="flex mb-10">
+				<div className="self-start sticky-top vertical mt-4 pt-4">
+					<h2 className="font-bold text-yellow-500">Articles</h2>
+				</div>
+				<div className="flex-col m-4">
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							The Watson Fellowship
+						</h1>
+						<div className="flex flex-row">
+							<h3 className="text-left font-plex group-hover:italic">
+								2019 — 2020
+							</h3>
+						</div>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Colander World
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Discussion Collective
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic rotat">
+							2019 — 2020
+						</h3>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Annals of Extraction
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+				</div>
+			</div>
+			<div className="flex mb-10">
+				<div className="self-start sticky-top vertical mt-4 pt-4">
+					<h2 className="font-bold text-yellow-500">Education</h2>
+				</div>
+				<div className="flex-col m-4">
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline underline">
+							The Watson Fellowship
+						</h1>
+						<div className="flex flex-row">
+							<h3 className="text-left font-plex group-hover:italic">
+								2019 — 2020
+							</h3>
+						</div>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Colander World
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Discussion Collective
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+					<div className="group">
+						<h1 className="text-5xl text-left group-hover:underline">
+							Annals of Extraction
+						</h1>
+						<h3 className="text-left font-plex group-hover:italic">
+							2019 — 2020
+						</h3>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#333',
-      },
-    },
-  },
-  variants: {},
-  plugins: [],
+	purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		fontFamily: {plex: ['IBM Plex Mono'], header: ['Russo One']},
+		extend: {
+			colors: {
+				'accent-1': '#333'
+			}
+		}
+	},
+	variants: {
+		textDecoration: ['hover', 'group-hover'],
+		fontStyle: ['hover', 'group-hover']
+	},
+	plugins: [require('@tailwindcss/typography')]
 }
